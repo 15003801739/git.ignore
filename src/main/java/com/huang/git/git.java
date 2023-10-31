9058a53a58fce7205dc473091f5f5a111732d268
ma <12345678912@qq.com>
2023-10-31 14:28:48 +0800

    first commit

---
+++ src/main/java/com/huang/git/git.java
@@ -0,0 +1,10 @@
+package com.huang.git;
+
+public class git {
+    public static void main(String[] args){
+        System.out.println("Hello git");
+        System.out.println("Hello git1");
+        System.out.println("Hello git2");
+        System.out.println("Hello git3");
+    }
+}
